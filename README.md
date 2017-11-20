321c6e938f37ab45e857b8d5b0a9b46de2d51c01
dcamposliz <dcampos.liz@gmail.com>
2017-11-19 22:31:43 -0800

    finished part 1/7: serialization

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Original docs:
 
-* [Serialization](http://www.django-rest-framework.org/tutorial/1-serialization/)
-* [Requests and Responses](http://www.django-rest-framework.org/tutorial/2-requests-and-responses/)
-* [Class-based Views](http://www.django-rest-framework.org/tutorial/3-class-based-views/)
-* [Authentication and Permissions](http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/)
-* [Relationships and Hyperlinked APIs](http://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/)
-* [ViewSets and Routers](http://www.django-rest-framework.org/tutorial/6-viewsets-and-routers/)
-* [Schemas and Client Libraries](http://www.django-rest-framework.org/tutorial/7-schemas-and-client-libraries/)
+* [1. Serialization](http://www.django-rest-framework.org/tutorial/1-serialization/)
+* [2. Requests and Responses](http://www.django-rest-framework.org/tutorial/2-requests-and-responses/)
+* [3. Class-based Views](http://www.django-rest-framework.org/tutorial/3-class-based-views/)
+* [4. Authentication and Permissions](http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/)
+* [5. Relationships and Hyperlinked APIs](http://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/)
+* [6. ViewSets and Routers](http://www.django-rest-framework.org/tutorial/6-viewsets-and-routers/)
+* [7. Schemas and Client Libraries](http://www.django-rest-framework.org/tutorial/7-schemas-and-client-libraries/)
---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    """
+    Defines SnippetSerializer as a function of Snippet model.
+    Notice that this is a shortcut, compared to the commented out code below.
+    ModelSerializers also provide simple implementations for the create()
+    and update() methods.
+    """
+    class Meta:
+        model = Snippet
+        fields = (
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style'
+        )
+
+# class SnippetSerializer(serializers.Serializer):
+#     """
+#     Defines the fields that get serialized/deserialized.
+#     """
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+#
+#     def create(self, validated_data):
+#         """
+#         Create and return a new `Snippet` instance, given the validated data.
+#         """
+#         return Snippet.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         """
+#         Update and return an existing `Snippet` instance, given the validated data.
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+
+# The serializer class is very similar to a Django `Form` class.
+# It includes similar validation flags on the various fields, as well as how data should be displayed.
